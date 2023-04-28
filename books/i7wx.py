4ae82bc6bee32941404bdc2805b1b76cab922aad
hhsd <75669175+hh-side@users.noreply.github.com>
2023-04-28 11:11:48 +0800

    Update i7wx.py

--- books/i7wx.py
+++ books/i7wx.py
@@ -1,3 +1,4 @@
+import time
 from html.parser import HTMLParser
 from utils.messages import *
 from utils.rss import RSSFeed
@@ -83,28 +84,35 @@ def parse(history_path='./'):
     chapters_parser = ChaptersHTMLParser()
     content_parser = ContentHTMLParser()
     for book in BOOK_LIST:
-        rss_feed = RSSFeed(book[0], '')
-        if os.path.exists(history_path + book[2]):
-            rss_feed.load(history_path + book[2])
-        response = requests.get(book[1], timeout=10)
-        html = response.text
-        chapters_parser.chapters = []
-        chapters_parser.feed(html)
-        for chapter in chapters_parser.chapters[0:5]:
-            if rss_feed.exist(chapter['name']) is False:
-                print(f'New chapter {chapter["name"]}')
-                content_parser.next = chapter['link']
-                content_parser.content = ''
-                while content_parser.next != '':
-                    response = requests.get(content_parser.next, timeout=10)
-                    html = response.text
-                    content_parser.feed(html)
-                rss_feed.add_item(chapter['name'],
-                                  content_parser.content.replace('本章未完，请点击下一页继续阅读》》', ''),
-                                  chapter['link'])
-                push_wechat_message('# {} - {}'.format(book[0], chapter['name']), link=chapter['link'])
-        print(book)
-        rss_feed.save(history_path + book[2])
+        try:
+            rss_feed = RSSFeed(book[0], '')
+            if os.path.exists(history_path + book[2]):
+                rss_feed.load(history_path + book[2])
+            response = requests.get(book[1], timeout=10)
+            html = response.text
+            chapters_parser.chapters = []
+            chapters_parser.feed(html)
+            for chapter in chapters_parser.chapters[0:5]:
+                if rss_feed.exist(chapter['name']) is False:
+                    print(f'New chapter {chapter["name"]}')
+                    try:
+                        content_parser.next = chapter['link']
+                        content_parser.content = ''
+                        while content_parser.next != '':
+                            time.sleep(2)
+                            response = requests.get(content_parser.next, timeout=10)
+                            html = response.text
+                            content_parser.feed(html)
+                        rss_feed.add_item(chapter['name'],
+                                          content_parser.content.replace('本章未完，请点击下一页继续阅读》》', ''),
+                                          chapter['link'])
+                        push_wechat_message('# {} - {}'.format(book[0], chapter['name']), link=chapter['link'])
+                    except Exception as ex:
+                        print(ex)
+            print(book)
+            rss_feed.save(history_path + book[2])
+        except Exception as ex:
+            print(ex)
 
 
 if __name__ == '__main__':
