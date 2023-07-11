2111593f23fb68f200a946eb0f8dad0399f8abc1
jupyterhub <jupyterhub@test.com>
2023-07-11 03:48:19 +0000

    update

--- utils/upload.py
+++ utils/upload.py
@@ -19,16 +19,15 @@ def upload_to_server(source_file_path: str, target_folder='/local/rss'):
             "Connection": "keep-alive",
             "Content-Length": "{}".format(os.path.getsize(source_file_path)),
             "Content-Type": "video/mp4",
-            "File-Path": "/alist/{}/{}".format(target_folder, source_file_path.split('/')[-1])
+            "File-Path": "/{}/{}".format(target_folder, source_file_path.split('/')[-1])
         }
         upload_url = "http://{}/alist/api/fs/put".format(UPLOAD_API_HOST)
         with open(source_file_path, 'rb') as vf:
             res = requests.put(upload_url, headers=headers, data=vf)
             if res.status_code == 200:
                 return True
-            print(res.text)
     return False
 
 
 if __name__ == '__main__':
-    print(upload_to_server('../rss/xhdtsgs.rss'))
+    print(upload_to_server('../rss/bqg_rdds.rss'))
