12643e28d4d8c330de974e51729d7ab1581dde55
JamesSun-LR <49747273+JamesSun-LR@users.noreply.github.com>
2024-03-14 17:49:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from books import i7wx, bqg
+from books import i7wx, bqg, 56kog
 
 
 if __name__ == '__main__':
-    i7wx.parse('./rss/')
-    bqg.parse('./rss/')
+    #i7wx.parse('./rss/')
+    #bqg.parse('./rss/')
+    56kog.parse('./rss/')
